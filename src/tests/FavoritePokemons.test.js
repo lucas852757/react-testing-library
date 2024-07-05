640323df51e93f886e4be0de18046ff0713f4e19
Lucas <bithome1@outlook.com>
2024-07-04 21:28:24 -0300

    test: Testando o Componente FavoritePokemons .

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -2,22 +2,11 @@ import React from 'react';
 import { Router } from 'react-router-dom';
 import { render, screen } from '@testing-library/react';
 import { createBrowserHistory } from 'history';
-import { FavoritePokemons } from '../components';
+import userEvent from '@testing-library/user-event';
+import pokemons from '../data';
 import App from '../App';
 
-// test('1', () => {
-//   const history = createBrowserHistory();
-
-//   render(
-//     <Router history={ history }>
-//       <App />
-//     </Router>,
-//   );
-//   history.push('/favorites');
-//   expect(screen.getByText('Favorite pokémons')).toBeInTheDocument();
-// });
-
-test('2', () => {
+test('Renders the message "No favorite pokemon found"', () => {
   const history = createBrowserHistory();
 
   render(
@@ -26,17 +15,11 @@ test('2', () => {
     </Router>,
   );
   history.push('/favorites');
-  let { notFound } = FavoritePokemons;
-  notFound = jest.fn();
-  notFound.mockReturnValue('No favorite pokemon found');
-
-  // console.log(spyNotFound.props.children.props.children);
-  expect(notFound()).toBe('No favorite pokemon found');
-
-  notFound.mockRestore();
+  expect(screen.getByText('No favorite pokemon found')).toBeInTheDocument();
 });
 
-test('', () => {
+test('Renders all favorite pokemons', async () => {
+  const NINE = 9;
   const history = createBrowserHistory();
 
   render(
@@ -44,20 +27,23 @@ test('', () => {
       <App />
     </Router>,
   );
-  history.push('/favorites');
-  const pokemon = { id: 1, name: '1', type: 'fire', value: '1', measurementUnit: '1' };
-  const pokemonReturn = { id: 1, name: '1', type: 'fire', value: '1', measurementUnit: '1' };
-  let { renderFavoritePokemon } = FavoritePokemons;
-  renderFavoritePokemon = jest.fn();
-  renderFavoritePokemon.mockReturnValue(pokemon);
-  // jest.spyOn(FavoritePokemons, 'renderFavoritePokemon');
-  // const spyRenderFavoritePokemon = FavoritePokemons.renderFavoritePokemon();
-  // pokemons.map((pokemon) => spyRenderFavoritePokemon(pokemon));
-  // console.log(spyRenderFavoritePokemon.props.children.props.children);
-  // expect(screen.getByTestId('pokemon-name')).toBeInTheDocument();
-  // expect(screen.getByTestId('pokemon-type')).toBeInTheDocument();
-  // expect(screen.getByTestId('pokemon-weight')).toBeInTheDocument();
-  expect(renderFavoritePokemon()).toStrictEqual(pokemonReturn);
 
-  renderFavoritePokemon.mockRestore();
+  pokemons.forEach((pokemon) => {
+    const { id, name } = pokemon;
+    const route = `/pokemons/${id}`;
+
+    history.push(route);
+    userEvent.click(screen.getByRole('checkbox'));
+    expect(screen.getByRole('checkbox').checked).toBe(true);
+
+    history.push('/favorites/');
+
+    expect(screen.getByAltText(`${name} sprite`)).toBeInTheDocument();
+    expect(screen.getByAltText(`${name} is marked as favorite`));
+  });
+  expect(screen.getAllByTestId('pokemon-name')).toHaveLength(NINE);
+  expect(screen.getAllByTestId('pokemon-type')).toHaveLength(NINE);
+  expect(screen.getAllByTestId('pokemon-weight')).toHaveLength(NINE);
+
+  expect(screen.getAllByRole('link', { name: 'More details' })).toHaveLength(NINE);
 });
